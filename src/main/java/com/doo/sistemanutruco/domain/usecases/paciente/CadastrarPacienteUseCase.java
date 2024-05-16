154fd7444831f414b23e57023895ee2d4350db6b
isaacnwt <isaac.andrade@aluno.ifsp.edu.br>
2024-05-15 22:03:53 -0300

    feat: CadastrarPacienteUseCase base

--- src/main/java/com/doo/sistemanutruco/domain/usecases/paciente/CadastrarPacienteUseCase.java
+++ src/main/java/com/doo/sistemanutruco/domain/usecases/paciente/CadastrarPacienteUseCase.java
@@ -1,4 +1,16 @@
 package com.doo.sistemanutruco.domain.usecases.paciente;
 
+import com.doo.sistemanutruco.domain.entities.paciente.Paciente;
+
 public class CadastrarPacienteUseCase {
+
+    private PacienteDAO pacienteDAO;
+
+    public CadastrarPacienteUseCase(PacienteDAO pacienteDAO) {
+        this.pacienteDAO = pacienteDAO;
+    }
+
+    public String cadastrar(Paciente paciente) {
+        return pacienteDAO.create(paciente);
+    }
 }
---
+++ src/main/java/com/doo/sistemanutruco/domain/usecases/paciente/PacienteDAO.java
@@ -0,0 +1,7 @@
+package com.doo.sistemanutruco.domain.usecases.paciente;
+
+import com.doo.sistemanutruco.domain.entities.paciente.Paciente;
+import com.doo.sistemanutruco.domain.usecases.utils.DAO;
+
+public interface PacienteDAO extends DAO<Paciente, String> {
+}
