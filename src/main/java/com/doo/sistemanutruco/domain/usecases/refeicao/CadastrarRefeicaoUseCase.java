fb767704071c0c10293c79d0851b4f8f3a99231a
Raphael Martins <raphael.chiuso@gmail.com>
2024-05-16 13:49:56 -0300

    feat: CadastrarRefeicaoUseCase e RefeicaoDAO bases implementadas.

---
+++ src/main/java/com/doo/sistemanutruco/domain/usecases/refeicao/CadastrarRefeicaoUseCase.java
@@ -0,0 +1,11 @@
+package com.doo.sistemanutruco.domain.usecases.refeicao;
+
+public class CadastrarRefeicaoUseCase {
+    private final RefeicaoDAO refeicaoDAO;
+    
+    public CadastrarRefeicaoUseCase(RefeicaoDAO refeicaoDAO){
+        this.refeicaoDAO = refeicaoDAO;
+    }
+
+
+}
---
+++ src/main/java/com/doo/sistemanutruco/domain/usecases/refeicao/RefeicaoDAO.java
@@ -0,0 +1,7 @@
+package com.doo.sistemanutruco.domain.usecases.refeicao;
+
+import com.doo.sistemanutruco.domain.entities.refeicao.Refeicao;
+import com.doo.sistemanutruco.domain.usecases.utils.DAO;
+
+public interface RefeicaoDAO extends DAO<Refeicao, Integer> {
+}
