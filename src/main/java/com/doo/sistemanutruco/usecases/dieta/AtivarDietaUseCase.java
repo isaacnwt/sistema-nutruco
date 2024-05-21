4868f30cb905f833fdcb10afae00c4a989515abc
Raphael Martins <raphael.chiuso@gmail.com>
2024-05-21 14:26:27 -0300

    feat: AtivarDietaUseCase classe implementada.

---
+++ src/main/java/com/doo/sistemanutruco/usecases/dieta/AtivarDietaUseCase.java
@@ -0,0 +1,31 @@
+package com.doo.sistemanutruco.usecases.dieta;
+
+import com.doo.sistemanutruco.entities.dieta.Dieta;
+
+public class AtivarDietaUseCase {
+    private final DietaDAO dietaDAO;
+
+    public AtivarDietaUseCase(DietaDAO dietaDAO){
+        this.dietaDAO = dietaDAO;
+    }
+
+    public boolean ativar(Dieta dieta){
+        if (!dieta.getInativo()){
+            throw new IllegalStateException("Dieta já ativa!");
+        }
+
+        dieta.setInativo(false);
+        return dietaDAO.update(dieta);
+    }
+
+    public boolean inativar(Dieta dieta){
+        if (dieta.getInativo()){
+            throw new IllegalStateException("Dieta já inativa!");
+        }
+
+        // Adicionar validação se a dieta estiver em uso (CDU006 - Fluxo Alt 1)
+
+        dieta.setInativo(true);
+        return dietaDAO.update(dieta);
+    }
+}
