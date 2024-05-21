e1a9fca0953c7b3fd06d21633cbac3f11ccadbdd
isaacnwt <isaacnewton02@hotmail.com>
2024-05-21 00:08:08 -0300

    feat: Main para testes

---
+++ src/main/java/com/doo/sistemanutruco/main/Main.java
@@ -0,0 +1,12 @@
+package com.doo.sistemanutruco.main;
+
+import com.doo.sistemanutruco.usecases.paciente.CadastrarPacienteUseCase;
+
+public class Main {
+
+    private static CadastrarPacienteUseCase cadastrarPacienteUseCase;
+
+    public static void main(String[] args) {
+
+    }
+}
