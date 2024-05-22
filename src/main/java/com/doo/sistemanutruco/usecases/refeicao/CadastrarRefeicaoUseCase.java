7b676ecec9d6e62c0060abcf052bbdba0a8f6e93
Raphael Martins <raphael.chiuso@gmail.com>
2024-05-22 20:05:32 -0300

    feat: CadastrarRefeicaoUseCase validação e implementação adicionada.

--- src/main/java/com/doo/sistemanutruco/usecases/refeicao/CadastrarRefeicaoUseCase.java
+++ src/main/java/com/doo/sistemanutruco/usecases/refeicao/CadastrarRefeicaoUseCase.java
@@ -1,5 +1,8 @@
 package com.doo.sistemanutruco.usecases.refeicao;
 
+import com.doo.sistemanutruco.entities.refeicao.Refeicao;
+import com.doo.sistemanutruco.usecases.utils.EntityAlreadyExistsException;
+
 public class CadastrarRefeicaoUseCase {
     private final RefeicaoDAO refeicaoDAO;
     
@@ -7,5 +10,14 @@ public class CadastrarRefeicaoUseCase {
         this.refeicaoDAO = refeicaoDAO;
     }
 
+    public String cadastrar(Refeicao refeicao){
+        RefeicaoValidator validator = new RefeicaoValidator();
+        validator.validar(refeicao);
 
+        if (refeicaoDAO.findByNome(refeicao.getNome()).isPresent()){
+            throw new EntityAlreadyExistsException("Refeição com mesmo nome já cadastrada");
+        }
+
+        return refeicaoDAO.create(refeicao);
+    }
 }
