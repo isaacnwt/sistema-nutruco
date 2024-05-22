95a7c5474e26fb6cdf81ff8ab7ffb2395d010f80
Raphael Martins <raphael.chiuso@gmail.com>
2024-05-22 20:01:27 -0300

    feat: classe de validação de dados de Refeicao adicionada.

---
+++ src/main/java/com/doo/sistemanutruco/usecases/refeicao/RefeicaoValidator.java
@@ -0,0 +1,31 @@
+package com.doo.sistemanutruco.usecases.refeicao;
+
+import com.doo.sistemanutruco.entities.refeicao.Refeicao;
+import com.doo.sistemanutruco.usecases.utils.NullEntityException;
+
+public class RefeicaoValidator {
+
+    public void validar(Refeicao refeicao){
+        if (refeicao == null){
+            throw new NullEntityException("Refeição não pode ser nula");
+        }
+        if (refeicao.getNome() == null || refeicao.getNome().isEmpty()){
+            throw new IllegalArgumentException("Nome da refeição não pode ser nulo ou vazio");
+        }
+        if (refeicao.getDescricao() == null || refeicao.getDescricao().isEmpty()){
+            throw new IllegalArgumentException("Descrição da refeição não pode ser nulo ou vazio");
+        }
+        if (refeicao.getObjetivo() == null || refeicao.getObjetivo().isEmpty()){
+            throw new IllegalArgumentException("Objetivo da refeição não pode ser nulo ou vazio");
+        }
+        if (refeicao.getAlimentos() == null || refeicao.getAlimentos().isEmpty()){
+            throw new IllegalArgumentException("Alimentos da refeição não podem ser nulos");
+        }
+        if (refeicao.getContemGluten() == null){
+            throw new IllegalArgumentException("Contém glúten da refeição não pode ser nulo");
+        }
+        if (refeicao.getContemLactose() == null){
+            throw new IllegalArgumentException("Contém lactose da refeição não pode ser nulo");
+        }
+    }
+}
