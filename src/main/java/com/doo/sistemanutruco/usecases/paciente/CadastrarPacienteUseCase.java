cedf7d1f89f6f6a61dee15733c8de45be76d21e7
Raphael Martins <raphael.chiuso@gmail.com>
2024-05-22 19:46:56 -0300

    refactor: alteração para utilização da classe PacienteValidator.

--- src/main/java/com/doo/sistemanutruco/usecases/paciente/CadastrarPacienteUseCase.java
+++ src/main/java/com/doo/sistemanutruco/usecases/paciente/CadastrarPacienteUseCase.java
@@ -12,32 +12,12 @@ public class CadastrarPacienteUseCase {
     }
 
     public String cadastrar(Paciente paciente) {
+        PacienteValidator pacienteValidator = new PacienteValidator();
+        pacienteValidator.validar(paciente);
 
-        validar(paciente);
-
-        return pacienteDAO.create(paciente);
-    }
-
-    private void validar(Paciente paciente) {
-        if (paciente == null)
-            throw new IllegalArgumentException("Paciente não pode ser null");
-        if (paciente.getCpf() == null || paciente.getCpf().isEmpty())
-            throw new IllegalArgumentException("Cpf do paciente é obrigatório");
-        if (paciente.getNome() == null || paciente.getNome().isEmpty())
-            throw new IllegalArgumentException("Nome do paciente é obrigatório");
-        if (paciente.getDataNascimento() == null)
-            throw new IllegalArgumentException("Data de nascimento do paciente é obrigatório");
-        if (paciente.getTelefone() == null)
-            throw new IllegalArgumentException("Telefone do paciente é obrigatório");
-        if (paciente.getEmail() == null || paciente.getEmail().isEmpty())
-            throw new IllegalArgumentException("Email do paciente é obrigatório");
-        if (paciente.getPeso() == null)
-            throw new IllegalArgumentException("Peso do paciente é obrigatório");
-        if (paciente.getAltura() == null)
-            throw new IllegalArgumentException("Altura do paciente é obrigatório");
-        if (paciente.getObjetivo() == null || paciente.getObjetivo().isEmpty())
-            throw new IllegalArgumentException("Objetivo do paciente é obrigatório");
         if (pacienteDAO.findByCpf(paciente.getCpf()).isPresent())
             throw new EntityAlreadyExistsException("Paciente já cadastrado");
+
+        return pacienteDAO.create(paciente);
     }
 }
