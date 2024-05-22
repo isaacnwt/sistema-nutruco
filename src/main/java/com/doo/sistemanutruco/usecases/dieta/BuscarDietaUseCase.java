871995d0710801869ccd924385bb11bc9c16434b
Raphael Martins <raphael.chiuso@gmail.com>
2024-05-22 19:32:44 -0300

    feat: BuscarDietaUseCase base adicionada.

---
+++ src/main/java/com/doo/sistemanutruco/usecases/dieta/BuscarDietaUseCase.java
@@ -0,0 +1,30 @@
+package com.doo.sistemanutruco.usecases.dieta;
+
+import com.doo.sistemanutruco.entities.dieta.Dieta;
+
+import java.util.List;
+import java.util.Optional;
+
+public class BuscarDietaUseCase {
+    private final DietaDAO dietaDAO;
+
+    public BuscarDietaUseCase(DietaDAO dietaDAO){
+        this.dietaDAO = dietaDAO;
+    }
+
+    public List<Dieta> findAll(){
+        return dietaDAO.findAll();
+    }
+
+    public Optional<Dieta> findByNome(String nome){
+        // Adicionar validações
+
+        return dietaDAO.findByNome(nome);
+    }
+
+    public Optional<Dieta> findByDescricao(String descricao){
+        // Adicionar validações
+
+        return dietaDAO.findByDescricao(descricao);
+    }
+}
