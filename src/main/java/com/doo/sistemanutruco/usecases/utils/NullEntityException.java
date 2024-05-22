42ceb975c65bd0b838ea459928e989fabf230950
Raphael Martins <raphael.chiuso@gmail.com>
2024-05-22 19:10:36 -0300

    feat: NullEntityException adicionada

---
+++ src/main/java/com/doo/sistemanutruco/usecases/utils/NullEntityException.java
@@ -0,0 +1,7 @@
+package com.doo.sistemanutruco.usecases.utils;
+
+public class NullEntityException extends RuntimeException{
+    public NullEntityException(String message) {
+        super(message);
+    }
+}
