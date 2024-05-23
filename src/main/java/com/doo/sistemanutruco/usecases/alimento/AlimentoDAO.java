8b56c1f500e4189f5cf8a0410c607466183c326d
Raphael Martins <raphael.chiuso@gmail.com>
2024-05-23 12:14:25 -0300

    feat: adição de método na AlimentoDAO.

--- src/main/java/com/doo/sistemanutruco/usecases/alimento/AlimentoDAO.java
+++ src/main/java/com/doo/sistemanutruco/usecases/alimento/AlimentoDAO.java
@@ -3,5 +3,8 @@ package com.doo.sistemanutruco.usecases.alimento;
 import com.doo.sistemanutruco.entities.alimento.Alimento;
 import com.doo.sistemanutruco.usecases.utils.DAO;
 
+import java.util.Optional;
+
 public interface AlimentoDAO extends DAO<Alimento, Integer> {
+    Optional<Alimento> findByNome(String nome);
 }
