c73436b21eef13769eb014bff71051aa936c5509
Raphael Martins <raphael.chiuso@gmail.com>
2024-05-23 12:41:52 -0300

    feat: alimento em memoria adicionado

---
+++ src/main/java/com/doo/sistemanutruco/repository/memory/InMemoryAlimentoDAO.java
@@ -0,0 +1,63 @@
+package com.doo.sistemanutruco.repository.memory;
+
+import com.doo.sistemanutruco.entities.alimento.Alimento;
+import com.doo.sistemanutruco.usecases.alimento.AlimentoDAO;
+
+import java.util.*;
+
+public class InMemoryAlimentoDAO implements AlimentoDAO {
+    public static final Map<Integer, Alimento> db = new LinkedHashMap<>();
+    public static int id = 0;
+
+    @Override
+    public Integer create(Alimento alimento) {
+        alimento.setId(++id);
+        db.put(id, alimento);
+        return id;
+    }
+
+    @Override
+    public Optional<Alimento> findOne(Integer key) {
+        if (db.containsKey(key))
+            return Optional.of(db.get(key));
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Alimento> findAll() {
+        return new ArrayList<>(db.values());
+    }
+
+    @Override
+    public boolean update(Alimento alimento) {
+        Integer id = alimento.getId();
+        if (db.containsKey(id)) {
+            db.replace(id, alimento);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<Alimento> findByNome(String nome) {
+        return db.values().stream()
+                .filter(alimento -> alimento.getNome().equals(nome))
+                .findAny();
+    }
+
+    @Override
+    public boolean deleteByKey(Integer key) {
+        if (db.containsKey(key)) {
+            db.remove(key);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(Alimento alimento) {
+        return deleteByKey(alimento.getId());
+    }
+}
+
+
