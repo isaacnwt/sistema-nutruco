6d53c7f422c4a24d4c739a2daaa8698a64d057b6
BrenoNLps <brenolopes.xy@gmail.com>
2024-05-31 17:32:47 -0300

    feat: atribuir dieta implementado

---
+++ src/main/java/com/doo/sistemanutruco/usecases/dieta/AtribuirDietaUseCase.java
@@ -0,0 +1,17 @@
+package com.doo.sistemanutruco.usecases.dieta;
+
+import com.doo.sistemanutruco.entities.dieta.Dieta;
+import com.doo.sistemanutruco.entities.paciente.Paciente;
+import com.doo.sistemanutruco.usecases.paciente.PacienteDAO;
+
+public class AtribuirDietaUseCase {
+    private final PacienteDAO pacienteDAO;
+
+    public AtribuirDietaUseCase(PacienteDAO pacienteDAO) {this.pacienteDAO = pacienteDAO;}
+
+    public boolean atribuirDieta(Paciente paciente, Dieta dieta){
+        paciente.setDieta(dieta);
+        return  pacienteDAO.update(paciente);
+    }
+
+}
