336fcea271f7963e7e9b005535b22f7b37118793
Raphael Martins <raphael.chiuso@gmail.com>
2024-06-03 08:37:17 -0300

    style: remoção do identificador dos UseCases devido a inconsistências na documentação

--- src/main/java/com/doo/sistemanutruco/main/Main.java
+++ src/main/java/com/doo/sistemanutruco/main/Main.java
@@ -63,7 +63,7 @@ public class Main {
         ativarPacienteUseCase = new AtivarPacienteUseCase(pacienteDAO);
 
         // CDU001 – Cadastrar paciente
-        System.out.println("CDU001 - Cadastrar Paciente");
+        System.out.println("Cadastrar Paciente/Buscar Paciente");
         Paciente paciente1 = new Paciente("123", "teste", LocalDate.of(2000, 7, 2),
                 16999998888L, "teste@email.com",90.0, 1.80, "ganhar massa");
         Paciente paciente2 = new Paciente("456", "teste2", LocalDate.of(2000, 8, 12),
@@ -71,16 +71,14 @@ public class Main {
 
         cadastrarPacienteUseCase.cadastrar(paciente1);
         cadastrarPacienteUseCase.cadastrar(paciente2);
-        System.out.println("--------------");
 
         // CDU0013 – Buscar Paciente
-        System.out.println("CDU0013 - Buscar Paciente");
         buscarPacienteUseCase.findByCpf("123").ifPresent(System.out::println);
         buscarPacienteUseCase.findByCpf("456").ifPresent(System.out::println);
         System.out.println("--------------");
 
         // CDU002 – Editar paciente
-        System.out.println("CDU0013 - Editar Paciente");
+        System.out.println("Editar Paciente");
         Paciente paciente1Update = new Paciente("123", "teste update", LocalDate.of(2000, 7, 2),
                 16999990000L, "teste@email.com",90.0, 1.82, "ganhar massa");
 
@@ -92,8 +90,7 @@ public class Main {
         System.out.println("--------------");
 
         // [RF003]/[RF004] Inativar/Ativar Paciente
-        System.out.println("CDU003 - Inativar Paciente");
-
+        System.out.println("Inativar Paciente");
         if (ativarPacienteUseCase.inativar(paciente2)) {
             System.out.println("Desativado com sucesso");
             buscarPacienteUseCase.findByCpf("456").ifPresent(System.out::println);
@@ -101,7 +98,7 @@ public class Main {
         else System.out.println("Erro ao desativar");
         System.out.println("--------------");
 
-        System.out.println("CDU004 - Ativar Paciente");
+        System.out.println("Ativar Paciente");
         if (ativarPacienteUseCase.ativar(paciente2)) {
             System.out.println("Ativado com sucesso");
             buscarPacienteUseCase.findByCpf("456").ifPresent(System.out::println);
@@ -124,7 +121,7 @@ public class Main {
         clonarDietaUseCase = new ClonarDietaUseCase(dietaDAO);
 
         // CDU003 – Importar alimentos
-        System.out.println("CDU003 - Importar Alimentos");
+        System.out.println("Importar Alimentos");
         importarAlimentoUseCase.importarAlimentosCSV("src/main/resources/com/doo/sistemanutruco/docs/alimentos.csv");
 
         List<Alimento> alimentos = alimentoDAO.findAll();
@@ -133,7 +130,7 @@ public class Main {
         System.out.println("-------------");
 
         // CDU0012 – Cadastrar Refeição
-        System.out.println("CDU0012 - Cadastrar Refeição");
+        System.out.println("Cadastrar Refeição");
         List<Alimento> cafeDaManhaAlimentos = new ArrayList<>();
         cafeDaManhaAlimentos.add(alimentos.get(3)); // Pão
         cafeDaManhaAlimentos.add(alimentos.get(7)); // Queijo
@@ -162,7 +159,7 @@ public class Main {
         System.out.println("--------------");
 
         // CDU0011 – Cadastrar Dia
-        System.out.println("CDU0011 - Cadastrar Dia");
+        System.out.println("Cadastrar Dia");
         List<Refeicao> refeicoesSegunda = new ArrayList<>();
         refeicoesSegunda.add(cafeDaManha);
         refeicoesSegunda.add(almoco);
@@ -172,14 +169,14 @@ public class Main {
         System.out.println("--------------");
 
         // CDU004 – Cadastrar dieta
-        System.out.println("CDU004 - Cadastrar Dieta");
+        System.out.println("Cadastrar Dieta");
         Dieta dieta1 = new Dieta("dieta1","ficar saudável", List.of(segunda));
         cadastrarDietaUseCase.cadastrar(dieta1);
         System.out.println(dietaDAO.findAll().get(0));
         System.out.println("--------------");
 
-        // CDU008 - Clonar dieta
-        System.out.println("CDU008 - Clonar Dieta");
+        // CDU005 - Clonar dieta
+        System.out.println("Clonar Dieta");
         Optional<Dieta> dietaClonada = clonarDietaUseCase.clonarDieta(dieta1);
         System.out.println(dietaClonada);
         System.out.println("--------------");
