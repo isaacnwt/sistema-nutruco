d2d8dd12602140fe004d55584c3b11ab786324ee
Raphael Martins <raphael.chiuso@gmail.com>
2024-06-26 11:11:17 -0300

    feat: implementação de tabela com todas as dietas no GestaoDieta

--- src/main/java/com/doo/sistemanutruco/controller/GestaoDietaController.java
+++ src/main/java/com/doo/sistemanutruco/controller/GestaoDietaController.java
@@ -1,23 +1,73 @@
 package com.doo.sistemanutruco.controller;
 
+import com.doo.sistemanutruco.entities.dieta.Dieta;
+import com.doo.sistemanutruco.entities.paciente.Paciente;
+import com.doo.sistemanutruco.repository.sqlite.SqliteDietaDAO;
+import com.doo.sistemanutruco.repository.sqlite.SqlitePacienteDAO;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 import static java.util.Arrays.stream;
 
 public class GestaoDietaController {
+    @FXML
+    private TableView<Dieta> dietasTableView;
+    @FXML
+    private TableColumn<Dieta, String> nomeColumn;
+    @FXML
+    private TableColumn<Dieta, String> objetivoColumn;
+    @FXML
+    private TableColumn<Dieta, Double> caloriasTotaisColumn;
+    @FXML
+    private TableColumn<Dieta, Double> carboidratosTotaisColumn;
+    @FXML
+    private TableColumn<Dieta, Double> proteinasTotaisColumn;
+    @FXML
+    private TableColumn<Dieta, Double> gordurasTotaisColumn;
+    @FXML
+    private TableColumn<Dieta, Double> sodioTotalColumn;
+    private final SqliteDietaDAO dietaDAO = new SqliteDietaDAO();
+    private ObservableList<Dieta> dietas;
     public GestaoDietaController() {
+    }
+
+    @FXML
+    public void initialize(){
+        loadDietas();
+        nomeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNome()));
+        objetivoColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getObjetivo()));
+        caloriasTotaisColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getCaloriasDaDieta()));
+        carboidratosTotaisColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getCarboidratosDaDieta()));
+        proteinasTotaisColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getProteinasDaDieta()));
+        gordurasTotaisColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getGordurasDaDieta()));
+        sodioTotalColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getSodioDaDieta()));
     }
 
     @FXML
     private void handleAtribuirDieta() {
         openAtribuirDietaView();
+    }
+
+    private void loadDietas() {
+        List<Dieta> dietasList = dietaDAO.findAll();
+        dietas = FXCollections.observableArrayList(dietasList);
+        dietasTableView.setItems(dietas);
     }
 
     private void openAtribuirDietaView() {
